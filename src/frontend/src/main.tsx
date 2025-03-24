c0db7cc0b3724197cf05b46453109a8d7b6b66d2
zerotype19 <kevin.mcgovern@gmail.com>
2025-03-23 21:08:15 -0400

    Load React App and router in main.tsx

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,27 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ChildProvider } from './contexts/ChildContext';
-import { DashboardPage } from './pages/DashboardPage';
-import { JournalPage } from './pages/JournalPage';
-import { ProgressDashboard } from './pages/ProgressDashboard';
-import { CalendarPage } from './pages/CalendarPage';
-import { ChallengeViewer } from './pages/ChallengeViewer';
-import './index.css';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChildProvider>
-        <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/journal/:child_id" element={<JournalPage />} />
-          <Route path="/progress/:child_id" element={<ProgressDashboard />} />
-          <Route path="/calendar/:child_id" element={<CalendarPage />} />
-          <Route path="/challenge/:id" element={<ChallengeViewer />} />
-        </Routes>
-      </ChildProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
-);
+)
