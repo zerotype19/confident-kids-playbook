f11760ef5891d8042dfb77f3596ca734e53bdbd9
zerotype19 <kevin.mcgovern@gmail.com>
2025-03-24 07:50:02 -0400

    Remove duplicate router to fix landing page routing

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { ChildProvider } from './contexts/ChildContext';
 import HomePage from './pages/HomePage';
@@ -16,81 +16,79 @@ import { ProfilePage } from './pages/ProfilePage';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <AuthProvider>
-        <ChildProvider>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route
-              path="/dashboard"
-              element={
-                <PrivateRoute>
-                  <DashboardPage />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/practice"
-              element={
-                <PrivateRoute>
-                  <PracticePage />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/calendar"
-              element={
-                <PrivateRoute>
-                  <CalendarPage />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/journal/:child_id"
-              element={
-                <PrivateRoute>
-                  <JournalPage />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/family-settings"
-              element={
-                <PrivateRoute>
-                  <FamilySettingsPage />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/progress/:child_id"
-              element={
-                <PrivateRoute>
-                  <ProgressDashboard />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/challenge/:child_id/:challenge_id"
-              element={
-                <PrivateRoute>
-                  <ChallengeViewer />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/profile"
-              element={
-                <PrivateRoute>
-                  <ProfilePage />
-                </PrivateRoute>
-              }
-            />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </ChildProvider>
-      </AuthProvider>
-    </Router>
+    <AuthProvider>
+      <ChildProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <DashboardPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/practice"
+            element={
+              <PrivateRoute>
+                <PracticePage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/calendar"
+            element={
+              <PrivateRoute>
+                <CalendarPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/journal/:child_id"
+            element={
+              <PrivateRoute>
+                <JournalPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/family-settings"
+            element={
+              <PrivateRoute>
+                <FamilySettingsPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/progress/:child_id"
+            element={
+              <PrivateRoute>
+                <ProgressDashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/challenge/:child_id/:challenge_id"
+            element={
+              <PrivateRoute>
+                <ChallengeViewer />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <ProfilePage />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </ChildProvider>
+    </AuthProvider>
   );
 };
 
