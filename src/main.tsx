150abab42b86ef5fd4bcd6adc29bef5d4806daf9
zerotype19 <kevin.mcgovern@gmail.com>
2025-03-28 22:04:43 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./frontend/src/App"
+import './index.css';
+
 
 console.log("✅ main.tsx loaded");
 
