658ddf4f8525909b666c750862a1a33e66914cf4
zerotype19 <kevin.mcgovern@gmail.com>
2025-04-04 17:37:19 -0400

    feat: link header logo to dashboard

--- src/frontend/src/components/PageWrapper.tsx
+++ src/frontend/src/components/PageWrapper.tsx
@@ -1,7 +1,7 @@
 // src/components/PageWrapper.tsx
 
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 import SubNavBar from './SubNavBar';
 
 export default function PageWrapper() {
@@ -9,11 +9,13 @@ export default function PageWrapper() {
     <div className="min-h-screen">
       <header className="bg-white border-b border-gray-200">
         <div className="max-w-screen-lg mx-auto px-4 py-4">
-          <img 
-            src="/logo.png" 
-            alt="Kidoova Logo" 
-            className="h-10 md:h-12"
-          />
+          <Link to="/dashboard">
+            <img 
+              src="/logo.png" 
+              alt="Kidoova Logo" 
+              className="h-10 md:h-12"
+            />
+          </Link>
         </div>
       </header>
       <SubNavBar />
