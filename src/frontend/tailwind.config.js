1df2e219386c4eac3c48bd3d21b0a03472d93d22
zerotype19 <kevin.mcgovern@gmail.com>
2025-04-05 08:23:22 -0400

    style: update welcome text with heading font

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 } 
