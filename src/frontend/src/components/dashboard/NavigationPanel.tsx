4ed2cb3984758a7a8d8365d7810b92c56cfac105
zerotype19 <kevin.mcgovern@gmail.com>
2025-05-09 16:11:55 -0400

    chore: commit changes to NavigationPanel.tsx

--- src/frontend/src/components/dashboard/NavigationPanel.tsx
+++ src/frontend/src/components/dashboard/NavigationPanel.tsx
@@ -38,7 +38,7 @@ export default function NavigationPanel(): JSX.Element {
           onClick={() => navigate('/all-challenges')}
           className="bg-blue-600 text-white rounded-full px-4 py-2 text-sm w-full"
         >
-          All Challenges
+          All Workouts
         </button>
 
         <button
